115ce556347ebf0639e8014f3462b7a4951a169e
MoniqueHeusinger <m.heusinger1821@gmail.com>
2024-02-13 17:01:06 +0100

    Style für Bootsübersicht bearbeitet: schmale Cards

--- frontend/src/components/BootCard.jsx
+++ frontend/src/components/BootCard.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import "./BootCardNew.scss";
 import { backendUrl } from "../api";
+import deleteIcon from "../assets/img/icons/icon-trash-white.png";
+import calenderIcon from "../assets/img/icons/icon-calender-white.png";
 
 // neu BasicAuth
 const BootCard = (props, { authorization, userProfileInfo }) => {
@@ -15,23 +17,34 @@ const BootCard = (props, { authorization, userProfileInfo }) => {
   };
   return (
     <article className="boat-card">
+      {/* Bild vom Boot */}
       <Link to={`/boat-detail/${props._id}`}>
         <img src={props.bildUrl} alt="boot" onError={props.errFunc} />
       </Link>
-      <div className="card-text-container">
-        <p className="boot-name">{props.name}</p>
-        <p className="boot-typ">{props.bootsart}</p>
-        <p className="baujahr">(Baujahr: {props.baujahr})</p>
-        <Link className="btn" to={`/add-reservierung/${props._id}`}>
-          reservieren
+
+      {/* Container für Boot-Daten (Name, Typ, Bj) */}
+      <Link to={`/boat-detail/${props._id}`}>
+        <div className="card-text-container">
+          <p className="boot-name">{props.name}</p>
+          <p className="boot-typ">{props.bootsart}</p>
+          <p className="baujahr">(Baujahr: {props.baujahr})</p>
+        </div>
+      </Link>
+      {/* Container für Action-Buttons (reservieren, login...) */}
+      <div className="action-container">
+        <Link className="btn btn-icon" to={`/add-reservierung/${props._id}`}>
+          {/* reservieren */}
+          <img src={calenderIcon} alt="reservieren" />
         </Link>
         {!authorization ? (
-          <Link to="/login" className="btn">
-            Login zum Löschen
+          <Link to="/login" className="btn btn-icon">
+            {/* Login zum Löschen */}
+            <img src={deleteIcon} alt="löschen" />
           </Link>
         ) : (
-          <a className="btn" onClick={deleteBoat}>
-            Boot löschen
+          <a className="btn btn-icon" onClick={deleteBoat}>
+            <img src={deleteIcon} alt="löschen" />
+            {/* Boot löschen */}
           </a>
         )}
       </div>
